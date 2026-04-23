061855017d1b29841471aa56819a28f13e73056b
Evelyn200103 <149028414+Evelyn200103@users.noreply.github.com>
2026-04-23 22:05:42 +0000

    feat: cambio mensaje de bienvenida

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Función principal: suma dos números
 function sumar(a, b) {
- return a + b;
+  return a + b;
 }
 
 // Servidor web minimalista
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hola Mundo desde mi Pipeline!\n');
+  res.end('Hola Arquitectos de Software!\n');
 });
 
 server.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
